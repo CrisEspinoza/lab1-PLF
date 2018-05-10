6354829a4fe9aab57dce1246f1ec30ad953d666d
CrisEspinoza <cristian.espinoza.s@usach.cl>
2018-05-10 17:45:37 -0300

    Agregando separación de archivos

---
+++ function.h
@@ -0,0 +1,8 @@
+//Acá tenemos lo escabezados de las funciones a ocupar
+
+
+void cortar(char* palabraNueva,char* palaraAntigua,int inicio,int final);
+int operadoresYSimbolos(char palabra);
+int palabraReservada(char* palabra);
+void revisar (char* palabra);
+void analizadoLexico(char* nombre);
